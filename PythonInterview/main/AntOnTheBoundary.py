f77ad1536da52c121079993c3f2f67165a98512f
Mohit Rane <mohit.rane30@gmail.com>
2024-02-18 12:52:28 +0000

    New Questions

---
+++ PythonInterview/main/AntOnTheBoundary.py
@@ -0,0 +1,59 @@
+"""
+
+An ant is on a boundary. It sometimes goes left and sometimes right.
+
+You are given an array of non-zero integers nums. The ant starts reading nums from the first element of it to its end. At each step, it moves according to the value of the current element:
+
+If nums[i] < 0, it moves left by -nums[i] units.
+If nums[i] > 0, it moves right by nums[i] units.
+Return the number of times the ant returns to the boundary.
+
+Notes:
+
+There is an infinite space on both sides of the boundary.
+We check whether the ant is on the boundary only after it has moved |nums[i]| units. In other words, if the ant crosses the boundary during its movement, it does not count.
+
+
+Example 1:
+
+Input: nums = [2,3,-5]
+Output: 1
+Explanation: After the first step, the ant is 2 steps to the right of the boundary.
+After the second step, the ant is 5 steps to the right of the boundary.
+After the third step, the ant is on the boundary.
+So the answer is 1.
+Example 2:
+
+Input: nums = [3,2,-3,-4]
+Output: 0
+Explanation: After the first step, the ant is 3 steps to the right of the boundary.
+After the second step, the ant is 5 steps to the right of the boundary.
+After the third step, the ant is 2 steps to the right of the boundary.
+After the fourth step, the ant is 2 steps to the left of the boundary.
+The ant never returned to the boundary, so the answer is 0
+"""
+
+
+def returnToBoundaryCount(nums):
+    total_steps = 0
+    touchedBoundry = 0
+    for step in range(0, len(nums)):
+        print("Move : ", nums[step])
+        if nums[step] > 0:
+            print("move right")
+            total_steps += nums[step]
+            if total_steps == 0:
+                print("Touched Boundry With Right Move ")
+                touchedBoundry = touchedBoundry + 1
+        else:
+            print("Move left : ", total_steps)
+            total_steps = total_steps + nums[step]
+            if total_steps == 0:
+                print("Touched Boundry With Left Move ")
+                touchedBoundry =  touchedBoundry + 1
+    return touchedBoundry
+
+
+# print(returnToBoundaryCount([3, 2, -3, -4]))
+# print(returnToBoundaryCount([2, 3, -5]))
+print(returnToBoundaryCount([-9, 9, 6, -6]))
--- PythonInterview/main/LongestCommonPrefix.py
+++ PythonInterview/main/LongestCommonPrefix.py
@@ -15,18 +15,19 @@ Explanation: There is no common prefix among the input strings.
 
 
 def longestCommonPrefix(listStr):
-
     if not listStr:
         return ""  # If the input list is empty, there is no common prefix.
 
     # Find the shortest string in the list (the minimum length determines the common prefix length).
     min_len = min(len(s) for s in listStr)
-    #min_len = (len(s) for s in listStr)
+    # min_len = (len(s) for s in listStr)
     common_prefix = ""
     for i in range(min_len):
         # Compare the current character of all strings with the character at the same position in the first string.
         current_char = listStr[0][i]
+
         for string in listStr:
+            print("Checking in string : ", string, "to :",current_char)
             if string[i] != current_char:
                 return common_prefix  # If characters don't match, return the common prefix found so far.
 
@@ -35,4 +36,5 @@ def longestCommonPrefix(listStr):
     return common_prefix
 
 
-
+listStr = ["flower", "flow", "flight"]
+print(longestCommonPrefix(listStr))
